b9284ac8740da74ff96791d5083e6b1bd602c51e
Jay Cortes <jaycortes@Jays-Air.fritz.box>
2024-03-04 15:39:34 +0100

    class on march

---
+++ main.js
@@ -0,0 +1,23 @@
+let numbers = [1, 2, 3, 4, 5];
+
+let students = [
+  {
+    name: "Jay",
+    grade: 3,
+    age: 33,
+  },
+  {
+    name: "Santiago",
+    grade: 10,
+    age: 27,
+  },
+];
+
+for (let index = 0; index < students.length; index++) {
+    const allStudents = students[index].name;
+    console.log(allStudents);
+};
+
+export {students}
+
+
---
+++ methods.js
@@ -0,0 +1,16 @@
+/* let numbers = [1,2,3,4,5];
+let otherNumbers = numbers.forEach(display);
+
+function display(item, index) {
+    console.log(item);
+};
+
+*/
+
+const studentsTwo = require("main.js")
+
+console.log(studentsTwo);
+
+map 
+filter 
+forEach
