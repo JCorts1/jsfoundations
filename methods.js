b3c58d0bf3a7bdac35cf58bbc6a062b81e05990b
Jay Cortes <jaycortes@Jays-Air.fritz.box>
2024-03-06 18:46:02 +0100

    This is the last update of all the methods proposed by my mentor Santiago Bedoya

--- methods.js
+++ methods.js
@@ -1,16 +1,5 @@
-/* let numbers = [1,2,3,4,5];
-let otherNumbers = numbers.forEach(display);
-
-function display(item, index) {
-    console.log(item);
-};
-
-filter 
-forEach
-*/
-
-/* map */
-
+// map
+ 
 const gastos = [
     { concepto: 'Comida', monto: 30.50, fecha: '2024-03-04' },
     { concepto: 'Transporte', monto: 20.00, fecha: '2024-03-03' },
@@ -18,11 +7,34 @@ const gastos = [
     { concepto: 'Facturas', monto: 100.00, fecha: '2024-03-01' }
   ];
 
-  const totalGastos = gastos.map((item) => item.monto).reduce((item, current) => item + current, 0);//gastos.reduce(sum)  ;
-  
-  // function sum(accumulator, element) {
-   // return accumulator + element;
-//}
-
-
+  const totalGastos = gastos.map((item) => item.monto).reduce((item, current) => item + current, 0);
   console.log(totalGastos);
+
+  //Filter
+
+const customers = [
+    {nameId: "Juan", age: 32},
+    {nameId: "Max", age: 17},
+    {nameId: "Dom", age: 18},
+    {nameId: "Kurl", age: 16},
+    {nameId: "Sam", age: 27},
+];
+
+const olders = customers.filter((item) => item.age >= 18);
+console.log(olders);
+
+//forEach
+
+const animals = [
+    {animal: "Cow"},
+    {animal: "Snake"},
+    {animal: "Deer"},
+    {animal: "Wild-dog"},
+];
+
+const message = "s in the"
+
+const warningAnimal = animals.forEach((item) => console.log(item.animal + "s in the area"));
+
+console.log(warningAnimal);
+
