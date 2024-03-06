82b99e759d620c3f823ea6266fc4770b9e7faee8
Jay Cortes <jaycortes@Jays-Air.fritz.box>
2024-03-06 17:56:01 +0100

    Add update

--- methods.js
+++ methods.js
@@ -5,12 +5,24 @@ function display(item, index) {
     console.log(item);
 };
 
-*/
-
-const studentsTwo = require("main.js")
-
-console.log(studentsTwo);
-
-map 
 filter 
 forEach
+*/
+
+/* map */
+
+const gastos = [
+    { concepto: 'Comida', monto: 30.50, fecha: '2024-03-04' },
+    { concepto: 'Transporte', monto: 20.00, fecha: '2024-03-03' },
+    { concepto: 'Entretenimiento', monto: 50.00, fecha: '2024-03-02' },
+    { concepto: 'Facturas', monto: 100.00, fecha: '2024-03-01' }
+  ];
+
+  const totalGastos = gastos.map((item) => item.monto).reduce((item, current) => item + current, 0);//gastos.reduce(sum)  ;
+  
+  // function sum(accumulator, element) {
+   // return accumulator + element;
+//}
+
+
+  console.log(totalGastos);
---
+++ practice.js
@@ -0,0 +1,9 @@
+const users = [
+    {name: "John", age: 34},
+    {name: "Amy", age: 20},
+    {name: "camperCat", age: 10},
+];
+
+const names = users.map((item) => item.name);
+
+console.log(names);
